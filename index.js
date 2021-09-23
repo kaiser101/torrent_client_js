ae45b7f161c6c8303d1b15c28938467ad1e66710
kaiser101 <kaiser101@gmail.com>
2021-09-23 16:03:46 +0530

    Add fs-extra and change logging

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ const fs = require("fs");
 const log4js = require("log4js");
 
 log4js.configure({
-    appenders: { torrent: { type: "file", filename: "torrent.log" } },
+    appenders: {
+        torrent: {
+            type: "dateFile",
+            pattern: "yyyy-MM-dd",
+            keepFileExt: true, //
+            maxLogSize: 1024 * 1024 * 10, //1024 * 1024 * 1 = 1M
+            backups: 2, //
+            alwaysIncludePattern: true, //
+            daysToKeep: 3, //
+            filename: "torrent.log",
+        },
+    },
     categories: { default: { appenders: ["torrent"], level: "info" } },
 });
 const logger = log4js.getLogger("torrent");
