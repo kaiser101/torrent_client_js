3bd4195e68bbbd660f028310acc1806f33b07fb2
kaiser101 <kaiser101@gmail.com>
2021-12-18 13:28:15 +0530

    Use promise based version of amqp

--- sendMessage.js
+++ sendMessage.js
@@ -1,29 +1,45 @@
 const logger = require("./logger");
-const amqp = require("amqplib/callback_api");
+// const amqp = require("amqplib/callback_api");
+const amqp = require("amqplib");
+
+// const sendMessage = (queue = "node_queue", msg = "Test message") => {
+//     amqp.connect("amqp://localhost", (error, connection) => {
+//         if (error) {
+//             throw error;
+//         }
+//         connection.createChannel((error1, channel) => {
+//             if (error1) {
+//                 throw error1;
+//             }
+
+//             channel.assertQueue(queue, {
+//                 durable: true,
+//             });
+//             channel.sendToQueue(queue, Buffer.from(msg), {
+//                 persistent: true,
+//             });
+//             logger.info("Sent '%s'", msg);
+//         });
+//         setTimeout(() => {
+//             connection.close();
+//             // process.exit(0);
+//         }, 500);
+//     });
+// };
 
 const sendMessage = (queue = "node_queue", msg = "Test message") => {
-    amqp.connect("amqp://localhost", (error, connection) => {
-        if (error) {
-            throw error;
-        }
-        connection.createChannel((error1, channel) => {
-            if (error1) {
-                throw error1;
-            }
+    const amqpconn = amqp.connect("amqp://localhost");
 
-            channel.assertQueue(queue, {
-                durable: true,
-            });
-            channel.sendToQueue(queue, Buffer.from(msg), {
-                persistent: true,
-            });
-            logger.info("Sent '%s'", msg);
-        });
-        setTimeout(() => {
-            connection.close();
-            // process.exit(0);
-        }, 500);
-    });
+    amqpconn
+        .then((connection) => connection.createChannel())
+        .then((channel) =>
+            channel
+                .assertQueue(queue)
+                .then((ok) => channel.sendToQueue(queue, Buffer.from(msg)))
+        )
+        .catch(console.warn);
+
+    logger.info("Sent '%s'", msg);
 };
 
 module.exports = sendMessage;
