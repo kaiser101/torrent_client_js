455634f804b4684ad0153301ad8935ddfe249b30
kaiser101 <kaiser101@gmail.com>
2021-12-18 22:54:01 +0530

    Remove commented code

--- sendMessage.js
+++ sendMessage.js
@@ -1,31 +1,5 @@
 const logger = require("./logger");
-// const amqp = require("amqplib/callback_api");
 const amqp = require("amqplib");
-
-// const sendMessage = (queue = "node_queue", msg = "Test message") => {
-//     amqp.connect("amqp://localhost", (error, connection) => {
-//         if (error) {
-//             throw error;
-//         }
-//         connection.createChannel((error1, channel) => {
-//             if (error1) {
-//                 throw error1;
-//             }
-
-//             channel.assertQueue(queue, {
-//                 durable: true,
-//             });
-//             channel.sendToQueue(queue, Buffer.from(msg), {
-//                 persistent: true,
-//             });
-//             logger.info("Sent '%s'", msg);
-//         });
-//         setTimeout(() => {
-//             connection.close();
-//             // process.exit(0);
-//         }, 500);
-//     });
-// };
 
 const sendMessage = (queue = "node_queue", msg = "Test message") => {
     const amqpconn = amqp.connect("amqp://localhost");
